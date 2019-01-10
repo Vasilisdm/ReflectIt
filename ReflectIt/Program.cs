104bec22166502781d593ce08f4748433a5404a1
vasilis <vasilisdmr@gmail.com>
2019-01-10 23:54:10 +0200

    Expiremented with MakeGenericType and GenericTypeArguments

--- ReflectIt/Program.cs
+++ ReflectIt/Program.cs
@@ -10,6 +10,31 @@ namespace ReflectIt
     {
         static void Main(string[] args)
         {
+
+            var employeeList = CreateCollection(typeof(List<>), typeof(Employee));
+            Console.WriteLine(employeeList.GetType().Name);
+
+            var genericArguments = employeeList.GetType().GenericTypeArguments;
+
+            foreach (var arg in genericArguments)
+            {
+                Console.WriteLine("[{0}]", arg.Name);
+            }
+
+            Console.WriteLine();
         }
+
+        private static object CreateCollection(Type collectionType, Type itemType)
+        {
+            var closedType = collectionType.MakeGenericType(itemType);
+            return Activator.CreateInstance(closedType);
+        }
+    }
+
+    public class Employee
+    {
+
+        public string Name { get; set; }
+
     }
 }
