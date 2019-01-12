c9d4482d7b586f859f218599b59e5f8819545403
vasilis <vasilisdmr@gmail.com>
2019-01-12 21:47:55 +0200

    Renamed the unitTests to IoCTests.

--- ReflectIt.Tests/IoCTests.cs
+++ ReflectIt.Tests/IoCTests.cs
@@ -4,7 +4,7 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 namespace ReflectIt.Tests
 {
     [TestClass]
-    public class UnitTest1
+    public class IoCTests
     {
         [TestMethod]
         public void TestMethod1()
